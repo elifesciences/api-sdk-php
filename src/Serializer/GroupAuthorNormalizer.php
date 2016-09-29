5013155188a230759bacf1a5a181b4decb0164e4
Chris Wilkinson <c.wilkinson@elifesciences.org>
2016-09-29 13:42:33 +0100

    Add AuthorNormalizer

--- src/Serializer/GroupAuthorNormalizer.php
+++ src/Serializer/GroupAuthorNormalizer.php
@@ -3,25 +3,19 @@
 namespace eLife\ApiSdk\Serializer;
 
 use eLife\ApiSdk\Collection\ArrayCollection;
-use eLife\ApiSdk\Model\Address;
+use eLife\ApiSdk\Model\Author;
 use eLife\ApiSdk\Model\AuthorEntry;
 use eLife\ApiSdk\Model\GroupAuthor;
 use eLife\ApiSdk\Model\PersonAuthor;
-use eLife\ApiSdk\Model\Place;
-use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
-use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
-use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
-use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
-use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
-final class GroupAuthorNormalizer implements NormalizerInterface, DenormalizerInterface, NormalizerAwareInterface, DenormalizerAwareInterface
+final class GroupAuthorNormalizer extends AuthorNormalizer
 {
-    use DenormalizerAwareTrait;
-    use NormalizerAwareTrait;
-
-    public function denormalize($data, $class, $format = null, array $context = []) :GroupAuthor
-    {
+    public function denormalizeAuthor(
+        array $data,
+        string $class,
+        string $format = null,
+        array $context = []
+    ) : Author {
         return new GroupAuthor(
             $data['name'],
             new ArrayCollection(array_map(function (array $person) use ($format, $context) {
@@ -30,17 +24,13 @@ final class GroupAuthorNormalizer implements NormalizerInterface, DenormalizerIn
             new ArrayCollection(array_map(function (array $group) use ($format, $context) {
                 return $this->denormalizer->denormalize($group, GroupAuthor::class, $format, $context);
             }, $data['groups'] ?? [])),
-            array_map(function (array $affiliation) use ($format, $context) {
-                return $this->denormalizer->denormalize($affiliation, Place::class, $format, $context);
-            }, $data['affiliations'] ?? []),
+            $data['affiliations'],
             $data['competingInterests'] ?? null,
             $data['contribution'] ?? null,
             $data['emailAddresses'] ?? [],
             $data['equalContributionGroups'] ?? [],
             $data['phoneNumbers'] = [],
-            array_map(function (array $address) use ($format, $context) {
-                return $this->denormalizer->denormalize($address, Address::class, $format, $context);
-            }, $data['postalAddresses'] ?? [])
+            $data['postalAddresses']
         );
     }
 
@@ -55,12 +45,10 @@ final class GroupAuthorNormalizer implements NormalizerInterface, DenormalizerIn
     /**
      * @param GroupAuthor $object
      */
-    public function normalize($object, $format = null, array $context = []) : array
+    protected function normalizeAuthor(Author $object, array $data, $format = null, array $context = []) : array
     {
-        $data = [
-            'type' => 'group',
-            'name' => $object->getName(),
-        ];
+        $data['type'] = 'group';
+        $data['name'] = $object->getName();
 
         if (count($object->getPeople())) {
             $data['people'] = $object->getPeople()->map(function (PersonAuthor $author) use ($format, $context) {
@@ -72,35 +60,6 @@ final class GroupAuthorNormalizer implements NormalizerInterface, DenormalizerIn
             $data['groups'] = $object->getGroups()->map(function (GroupAuthor $author) use ($format, $context) {
                 return $this->normalizer->normalize($author, $format, $context);
             });
-        }
-
-        if (count($object->getAffiliations())) {
-            $data['affiliations'] = array_map(function (Place $place) use ($format, $context) {
-                return $this->normalizer->normalize($place, $format, $context);
-            }, $object->getAffiliations());
-        }
-
-        if ($object->getCompetingInterests()) {
-            $data['competingInterests'] = $object->getCompetingInterests();
-        }
-        if ($object->getContribution()) {
-            $data['contribution'] = $object->getContribution();
-        }
-
-        if (count($object->getEmailAddresses())) {
-            $data['emailAddresses'] = $object->getEmailAddresses();
-        }
-
-        if (count($object->getEqualContributionGroups())) {
-            $data['equalContributionGroups'] = $object->getEqualContributionGroups();
-        }
-
-        if (count($object->getPhoneNumbers())) {
-            $data['phoneNumbers'] = $object->getPhoneNumbers();
-        }
-
-        if (count($object->getPostalAddresses())) {
-            $data['postalAddresses'] = $object->getPostalAddresses();
         }
 
         return $data;
--- src/Serializer/PersonAuthorNormalizer.php
+++ src/Serializer/PersonAuthorNormalizer.php
@@ -2,39 +2,29 @@
 
 namespace eLife\ApiSdk\Serializer;
 
-use eLife\ApiSdk\Model\Address;
+use eLife\ApiSdk\Model\Author;
 use eLife\ApiSdk\Model\AuthorEntry;
 use eLife\ApiSdk\Model\Person;
 use eLife\ApiSdk\Model\PersonAuthor;
-use eLife\ApiSdk\Model\Place;
-use Symfony\Component\Serializer\Normalizer\DenormalizerAwareInterface;
-use Symfony\Component\Serializer\Normalizer\DenormalizerAwareTrait;
-use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
-use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
-use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
-use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
-final class PersonAuthorNormalizer implements NormalizerInterface, DenormalizerInterface, NormalizerAwareInterface, DenormalizerAwareInterface
+final class PersonAuthorNormalizer extends AuthorNormalizer
 {
-    use DenormalizerAwareTrait;
-    use NormalizerAwareTrait;
-
-    public function denormalize($data, $class, $format = null, array $context = []) : PersonAuthor
-    {
+    protected function denormalizeAuthor(
+        array $data,
+        string $class,
+        string $format = null,
+        array $context = []
+    ) : Author {
         return new PersonAuthor(
             $this->denormalizer->denormalize($data, Person::class, $format, $context),
             $data['deceased'] ?? false,
-            array_map(function (array $affiliation) use ($format, $context) {
-                return $this->denormalizer->denormalize($affiliation, Place::class, $format, $context);
-            }, $data['affiliations'] ?? []),
+            $data['affiliations'],
             $data['competingInterests'] ?? null,
             $data['contribution'] ?? null,
             $data['emailAddresses'] ?? [],
             $data['equalContributionGroups'] ?? [],
             $data['phoneNumbers'] = [],
-            array_map(function (array $address) use ($format, $context) {
-                return $this->denormalizer->denormalize($address, Address::class, $format, $context);
-            }, $data['postalAddresses'] ?? [])
+            $data['postalAddresses']
         );
     }
 
@@ -49,14 +39,12 @@ final class PersonAuthorNormalizer implements NormalizerInterface, DenormalizerI
     /**
      * @param PersonAuthor $object
      */
-    public function normalize($object, $format = null, array $context = []) : array
+    protected function normalizeAuthor(Author $object, array $data, $format = null, array $context = []) : array
     {
-        $data = [
-            'type' => 'person',
-            'name' => [
-                'preferred' => $object->getPreferredName(),
-                'index' => $object->getIndexName(),
-            ],
+        $data['type'] = 'person';
+        $data['name'] = [
+            'preferred' => $object->getPreferredName(),
+            'index' => $object->getIndexName(),
         ];
 
         if ($object->getOrcid()) {
@@ -65,35 +53,6 @@ final class PersonAuthorNormalizer implements NormalizerInterface, DenormalizerI
 
         if ($object->isDeceased()) {
             $data['deceased'] = $object->isDeceased();
-        }
-
-        if (count($object->getAffiliations())) {
-            $data['affiliations'] = array_map(function (Place $place) use ($format, $context) {
-                return $this->normalizer->normalize($place, $format, $context);
-            }, $object->getAffiliations());
-        }
-
-        if ($object->getCompetingInterests()) {
-            $data['competingInterests'] = $object->getCompetingInterests();
-        }
-        if ($object->getContribution()) {
-            $data['contribution'] = $object->getContribution();
-        }
-
-        if (count($object->getEmailAddresses())) {
-            $data['emailAddresses'] = $object->getEmailAddresses();
-        }
-
-        if (count($object->getEqualContributionGroups())) {
-            $data['equalContributionGroups'] = $object->getEqualContributionGroups();
-        }
-
-        if (count($object->getPhoneNumbers())) {
-            $data['phoneNumbers'] = $object->getPhoneNumbers();
-        }
-
-        if (count($object->getPostalAddresses())) {
-            $data['postalAddresses'] = $object->getPostalAddresses();
         }
 
         return $data;

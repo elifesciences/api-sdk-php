ac5dd7fa695eef84e32bc7cd7fd2605d32e80418
Giorgio Sironi <g.sironi@elifesciences.org>
2017-03-09 10:16:53 +0000

    Loading all articles script also tries to load references
    This cause an additional GET to the latest version of each published article

--- scripts/load_all_articles.php
+++ scripts/load_all_articles.php
@@ -28,7 +28,7 @@ $guzzle = new GuzzleHttp\Client([
 // Api SDK.
 $client = new eLife\ApiClient\HttpClient\BatchingHttpClient(
     new eLife\ApiClient\HttpClient\Guzzle6HttpClient($guzzle),
-    100
+    20
 );
 $sdk = new eLife\ApiSdk\ApiSdk($client);
 
@@ -43,6 +43,13 @@ foreach ($articles as $a) {
         continue;
     }
     echo "Article id: {$a->getId()}", PHP_EOL;
+    try {
+        if ($a instanceof eLife\ApiSdk\Model\ArticleVoR) {
+            echo "References: ", count($a->getReferences()), PHP_EOL;
+        }
+    } catch (Exception $e) {
+        var_dump($e->getMessage());
+    }
     //$a->getCopyright();
     //echo 'Article copyright loaded', PHP_EOL;
     ++$articlesCount;

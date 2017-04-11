ef2af11790d64cf62a5be00ce4dd70efc40e480c
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-04-11 09:22:59 +0100

    Turn nest blocks into a flattened sequence

---
+++ src/Model/Collection/FlattenedBlockSequence.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace eLife\ApiSdk\Model\Collection;
+
+use eLife\ApiSdk\CanBeCounted;
+use eLife\ApiSdk\Collection;
+use eLife\ApiSdk\Collection\ArraySequence;
+use eLife\ApiSdk\Collection\Sequence;
+use eLife\ApiSdk\ImmutableArrayAccess;
+use eLife\ApiSdk\Model\Block;
+use eLife\ApiSdk\Model\HasContent;
+use IteratorAggregate;
+use IteratorIterator;
+use Traversable;
+
+final class FlattenedBlockSequence implements IteratorAggregate, Sequence
+{
+    use CanBeCounted;
+    use ImmutableArrayAccess;
+
+    private $sequence;
+
+    public function __construct(Sequence $sequence)
+    {
+        $blockFinder = function (array $array, Block $block) use (&$blockFinder) {
+            $array[] = $block;
+            if ($block instanceof HasContent) {
+                $array = $block->getContent()->reduce($blockFinder, $array);
+            }
+
+            return $array;
+        };
+
+        $this->sequence = new ArraySequence($sequence->reduce($blockFinder, []));
+    }
+
+    public function getIterator() : Traversable
+    {
+        return new IteratorIterator($this->sequence);
+    }
+
+    public function count() : int
+    {
+        return $this->sequence->count();
+    }
+
+    public function toArray() : array
+    {
+        return $this->sequence->toArray();
+    }
+
+    public function slice(int $offset, int $length = null) : Sequence
+    {
+        return new self($this->sequence->slice($offset, $length));
+    }
+
+    public function map(callable $callback) : Sequence
+    {
+        return new self($this->sequence->map($callback));
+    }
+
+    public function filter(callable $callback = null) : Collection
+    {
+        return new self($this->sequence->filter($callback));
+    }
+
+    public function reduce(callable $callback, $initial = null)
+    {
+        return $this->sequence->reduce($callback, $initial);
+    }
+
+    public function sort(callable $callback = null) : Sequence
+    {
+        return new self($this->sequence->sort($callback));
+    }
+
+    public function reverse() : Sequence
+    {
+        return new self($this->sequence->reverse());
+    }
+
+    public function offsetExists($offset) : bool
+    {
+        return $this->sequence->offsetExists($offset);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->sequence->offsetGet($offset);
+    }
+}

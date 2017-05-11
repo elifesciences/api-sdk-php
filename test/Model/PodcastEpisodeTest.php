6e8faa6eb1583e67570f2eeca91868c19b7b66e9
Chris Wilkinson <c.wilkinson@elifesciences.org>
2017-05-11 13:52:44 +0100

    Remove podcast episode full title

--- test/Model/PodcastEpisodeTest.php
+++ test/Model/PodcastEpisodeTest.php
@@ -67,7 +67,6 @@ final class PodcastEpisodeTest extends PHPUnit_Framework_TestCase
             new PromiseSequence(rejection_for('Chapters should not be unwrapped')));
 
         $this->assertSame('title', $podcastEpisode->getTitle());
-        $this->assertSame('Episode 1: title', $podcastEpisode->getFullTitle());
     }
 
     /**

d31b8e203c381312dd8e5b6bc5b06111daf000fd
Chris Wilkinson <chriswilkinson84@gmail.com>
2017-05-25 11:32:29 +0100

    Update highlights for pagination (#150)

--- src/Client/Highlights.php
+++ src/Client/Highlights.php
@@ -8,6 +8,7 @@ use eLife\ApiClient\Result;
 use eLife\ApiSdk\Collection\PromiseSequence;
 use eLife\ApiSdk\Collection\Sequence;
 use eLife\ApiSdk\Model\Highlight;
+use Iterator;
 use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 
 final class Highlights
@@ -23,16 +24,64 @@ final class Highlights
 
     public function get(string $id) : Sequence
     {
-        return new PromiseSequence($this->highlightsClient
-            ->list(
-                ['Accept' => new MediaType(HighlightsClient::TYPE_HIGHLIGHTS, 1)],
-                $id
-            )
-            ->then(function (Result $result) {
-                return array_map(function (array $item) {
-                    return $this->denormalizer->denormalize($item, Highlight::class);
-                }, $result->toArray());
-            })
-        );
+        $highlightsClient = $this->highlightsClient;
+        $denormalizer = $this->denormalizer;
+
+        return new class($highlightsClient, $denormalizer, $id) implements Iterator, Sequence {
+            use Client;
+
+            private $count;
+            private $descendingOrder = true;
+            private $highlightsClient;
+            private $denormalizer;
+            private $id;
+
+            public function __construct(HighlightsClient $highlightsClient, DenormalizerInterface $denormalizer, string $id)
+            {
+                $this->highlightsClient = $highlightsClient;
+                $this->denormalizer = $denormalizer;
+                $this->id = $id;
+            }
+
+            public function slice(int $offset, int $length = null) : Sequence
+            {
+                if (null === $length) {
+                    return new PromiseSequence($this->all()
+                        ->then(function (Sequence $sequence) use ($offset) {
+                            return $sequence->slice($offset);
+                        })
+                    );
+                }
+
+                return new PromiseSequence($this->highlightsClient
+                    ->listHighlights(
+                        ['Accept' => new MediaType(HighlightsClient::TYPE_HIGHLIGHT_LIST, 1)],
+                        $this->id,
+                        ($offset / $length) + 1,
+                        $length,
+                        $this->descendingOrder
+                    )
+                    ->then(function (Result $result) {
+                        $this->count = $result['total'];
+
+                        return $result;
+                    })
+                    ->then(function (Result $result) {
+                        return array_map(function (array $article) {
+                            return $this->denormalizer->denormalize($article, Highlight::class);
+                        }, $result['items']);
+                    })
+                );
+            }
+
+            public function reverse() : Sequence
+            {
+                $clone = clone $this;
+
+                $clone->descendingOrder = !$this->descendingOrder;
+
+                return $clone;
+            }
+        };
     }
 }

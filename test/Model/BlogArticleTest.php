1df029d6b6c5ba004d3d39dbe21f944bf486d15a
Chris Wilkinson <c.wilkinson@elifesciences.org>
2018-08-01 11:23:12 -0400

    Update usage

--- test/Model/BlogArticleTest.php
+++ test/Model/BlogArticleTest.php
@@ -178,7 +178,7 @@ final class BlogArticleTest extends PHPUnit_Framework_TestCase
         $content = [
             new Block\Paragraph('foo'),
             new Block\Image(null, null, new EmptySequence(), Builder::for(Image::class)->__invoke()),
-            new Block\YouTube('foo', 300, 200),
+            new Block\YouTube('foo', null, new EmptySequence(), 300, 200),
         ];
 
         $blogArticle = new BlogArticle('id', 'title', new DateTimeImmutable('now', new DateTimeZone('Z')), null, null, new ArraySequence($content),

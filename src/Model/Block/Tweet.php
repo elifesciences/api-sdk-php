cbc2f0b9245e4e95ec2537503fba5ea1b0bd782f
nlisgo <nathan@aeopus.com>
2020-04-23 10:57:39 +0100

    Add tweet block model

---
+++ src/Model/Block/Tweet.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace eLife\ApiSdk\Model\Block;
+
+use DateTimeImmutable;
+use eLife\ApiSdk\Model\Block;
+use eLife\ApiSdk\Model\HasId;
+
+final class Tweet implements HasId
+{
+    private $id;
+    private $date;
+    private $text;
+    private $accountId;
+    private $accountLabel;
+    private $conversation;
+    private $mediaCard;
+
+    /**
+     * @internal
+     */
+    public function __construct(
+        string $id,
+        DateTimeImmutable $date,
+        array $text,
+        string $accountId,
+        string $accountLabel,
+        bool $conversation = false,
+        bool $mediaCard = false
+    ) {
+        $this->id = $id;
+        $this->date = $date;
+        $this->text = $text;
+        $this->accountId = $accountId;
+        $this->accountLabel = $accountLabel;
+        $this->conversation = $conversation;
+        $this->mediaCard = $mediaCard;
+    }
+
+    public function getId() : string
+    {
+        return $this->id;
+    }
+
+    public function getDate() : DateTimeImmutable
+    {
+        return $this->date;
+    }
+
+    /**
+     * @return Block[]
+     */
+    public function getText() : array
+    {
+        return $this->text;
+    }
+
+    public function getAccountId() : string
+    {
+        return $this->accountId;
+    }
+
+    public function getAccountLabel() : string
+    {
+        return $this->accountLabel;
+    }
+
+    public function isConversation() : bool
+    {
+        return $this->conversation;
+    }
+
+    public function isMediaCard() : bool
+    {
+        return $this->mediaCard;
+    }
+}
---
+++ test/Model/Block/TweetTest.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace test\eLife\ApiSdk\Model\Block;
+
+use DateTimeImmutable;
+use DateTimeZone;
+use eLife\ApiSdk\Model\Block;
+use eLife\ApiSdk\Model\Block\Paragraph;
+use eLife\ApiSdk\Model\Block\Tweet;
+use eLife\ApiSdk\Model\HasId;
+use eLife\ApiSdk\Model\HasPublishedDate;
+use PHPUnit_Framework_TestCase;
+
+final class TweetTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @test
+     */
+    public function it_is_a_block()
+    {
+        $tweet = new Tweet('foo', new DateTimeImmutable('now', new DateTimeZone('Z')), [new Paragraph('tweet')], 'accountId', 'accountLabel');
+
+        $this->assertInstanceOf(Block::class, $tweet);
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_an_id()
+    {
+        $tweet = new Tweet('foo', new DateTimeImmutable('now', new DateTimeZone('Z')), [new Paragraph('tweet')], 'accountId', 'accountLabel');
+
+        $this->assertInstanceOf(HasId::class, $tweet);
+        $this->assertSame('foo', $tweet->getId());
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_a_date()
+    {
+        $tweet = new Tweet('foo',  $date = new DateTimeImmutable('now', new DateTimeZone('Z')), [new Paragraph('tweet')], 'accountId', 'accountLabel');
+
+        $this->assertInstanceOf(HasPublishedDate::class, $tweet);
+        $this->assertEquals($date, $tweet->getDate());
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_text()
+    {
+        $tweet = new Tweet('foo',  new DateTimeImmutable('now', new DateTimeZone('Z')), [new Paragraph('tweet')], 'accountId', 'accountLabel');
+
+        $this->assertEquals([new Paragraph('tweet')], $tweet->getText());
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_an_account_id()
+    {
+        $tweet = new Tweet('foo',  new DateTimeImmutable('now', new DateTimeZone('Z')), [new Paragraph('tweet')], 'accountId', 'accountLabel');
+
+        $this->assertEquals('accountId', $tweet->getAccountId());
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_an_account_label()
+    {
+        $tweet = new Tweet('foo',  new DateTimeImmutable('now', new DateTimeZone('Z')), [new Paragraph('tweet')], 'accountId', 'accountLabel');
+
+        $this->assertEquals('accountLabel', $tweet->getAccountLabel());
+    }
+
+    /**
+     * @test
+     */
+    public function it_may_be_a_conversation()
+    {
+        $true = new Tweet('foo',  new DateTimeImmutable('now', new DateTimeZone('Z')), [new Paragraph('tweet')], 'accountId', 'accountLabel', true);
+        $false = new Tweet('foo',  new DateTimeImmutable('now', new DateTimeZone('Z')), [new Paragraph('tweet')], 'accountId', 'accountLabel');
+
+        $this->assertTrue($true->isConversation());
+        $this->assertFalse($false->isConversation());
+    }
+
+    /**
+     * @test
+     */
+    public function it_may_contain_a_media_card()
+    {
+        $true = new Tweet('foo',  new DateTimeImmutable('now', new DateTimeZone('Z')), [new Paragraph('tweet')], 'accountId', 'accountLabel', false, true);
+        $false = new Tweet('foo',  new DateTimeImmutable('now', new DateTimeZone('Z')), [new Paragraph('tweet')], 'accountId', 'accountLabel', false);
+
+        $this->assertTrue($true->isMediaCard());
+        $this->assertFalse($false->isMediaCard());
+    }
+}

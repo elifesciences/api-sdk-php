9b55635d7853c2c492b82b65a12d5acecda410c9
nlisgo <nathan@aeopus.com>
2021-07-08 15:16:47 +0100

    Change default value of thumbnail on ArticlePoA

--- test/Builder.php
+++ test/Builder.php
@@ -306,7 +306,7 @@ final class Builder
                         'emailAddresses' => new EmptySequence(),
                     ];
                 },
-                ArticlePoA::class => $articlePoA = function () {
+                ArticlePoA::class => function () {
                     return [
                         'id' => '14107',
                         'stage' => 'published',
@@ -321,7 +321,7 @@ final class Builder
                         'statusDate' => new DateTimeImmutable('2016-03-28T00:00:00Z'),
                         'volume' => 5,
                         'elocationId' => 'e14107',
-                        'thumbnail' => self::for(Image::class)->sample('thumbnail'),
+                        'thumbnail' => null,
                         'socialImage' => self::for(Image::class)->sample('social'),
                         'pdf' => null,
                         'xml' => promise_for('http://www.example.com/xml'),

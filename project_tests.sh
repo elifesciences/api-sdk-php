acaca09148fd31da03c01e9f0bc00c9bf954d311
Giorgio Sironi <g.sironi@elifesciences.org>
2024-12-12 15:28:33 +0000

    Stop running phpcs twice in each build

--- project_tests.sh
+++ project_tests.sh
@@ -2,6 +2,5 @@
 #!/bin/bash
 set -e
 
-vendor/bin/phpcs --standard=phpcs.xml.dist --warning-severity=0 -p src/ scripts/ test/ spec/
 vendor/bin/phpunit --log-junit="build/${dependencies}-phpunit.xml"
 vendor/bin/phpspec run

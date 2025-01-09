b45b78fbe51e2a9d545dff88879c64a183303583
LinaKind <l.kinduryte@elifesciences.org>
2025-01-09 15:42:13 +0000

    Cover the new getter with a test
    https://github.com/elifesciences/issues/issues/9034

--- test/Model/ElifeAssessmentTest.php
+++ test/Model/ElifeAssessmentTest.php
@@ -2,12 +2,44 @@
 
 namespace test\eLife\ApiSdk\Model\Block;
 
+use eLife\ApiSdk\Collection\ArraySequence;
+use eLife\ApiSdk\Model\ArticleSection;
 use eLife\ApiSdk\Model\ElifeAssessment;
+use eLife\ApiSdk\Model\Block\Paragraph;
 use PHPUnit\Framework\TestCase;
 
 final class ElifeAssessmentTest extends TestCase
 {
     private $title = 'eLife Assessment';
+    private $articleSection;
+
+    /**
+     * @before
+     */
+    public function set_up()
+    {
+        $this->articleSection = new ArticleSection(new ArraySequence([new Paragraph('eLife assessment')]));
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_a_title()
+    {
+        $assessment = new ElifeAssessment($this->title, null, null, null);
+
+        $this->assertSame($this->title, $assessment->getTitle());
+    }
+
+    /**
+     * @test
+     */
+    public function it_has_an_article_section()
+    {
+        $assessment = new ElifeAssessment($this->title, $this->articleSection, null, null);
+
+        $this->assertSame($this->articleSection, $assessment->getArticleSection());
+    }
 
     /**
      * @test
@@ -31,15 +63,5 @@ final class ElifeAssessmentTest extends TestCase
 
         $this->assertSame(['solid'], $with->getStrength());
         $this->assertNull($withOut->getStrength());
-    }
-
-    /**
-     * @test
-     */
-    public function it_has_a_title()
-    {
-        $assessment = new ElifeAssessment($this->title, null, null, null);
-
-        $this->assertSame($this->title, $assessment->getTitle());
     }
 }

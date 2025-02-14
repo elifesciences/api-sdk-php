3246ea2bb1ed36ae65983e7683cb66ab1fe30dbb
David Moulton <d.moulton@elifesciences.org>
2025-02-14 14:04:28 +0000

    Implement another characterisation test
    https://github.com/elifesciences/issues/issues/9169

--- test/Client/SearchTest.php
+++ test/Client/SearchTest.php
@@ -142,6 +142,17 @@ class SearchTest extends ApiTestCase
     /**
      * @test
      */
+    public function it_can_handle_a_sequence_of_multiple_calls_with_different_subjects()
+    {
+        $this->mockCountCall(5, $query = '', $descendingOrder = true, ['biochemistry', 'neuroscience']);
+        $this->mockFirstPageCall(5, $query = '', $descendingOrder = true, ['biochemistry', 'neuroscience']);
+
+        $this->assertSame(5, $this->traverseAndSanityCheck($this->search->forSubject('biochemistry')->forSubject('neuroscience')));
+    }
+
+    /**
+     * @test
+     */
     public function it_can_be_filtered_by_elife_assessment_significance()
     {
         $this->mockCountCall(
